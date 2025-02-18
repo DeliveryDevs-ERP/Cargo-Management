2a75061736de550a27250617e70cf4a9e4b18553
OsamaASidd <osama.siddiqui2017@gmail.com>
2025-02-18 17:55:32 +0500

    rail pnl

---
+++ cargo_management/cargo_management/report/rail_pnl/rail_pnl.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2025, Osama and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Rail PNL"] = {
+	"filters": [
+		{
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Datetime", 
+            "default": frappe.datetime.add_months(frappe.datetime.now_datetime(), -12), 
+            "reqd": 0 
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("to Date"),
+            "fieldtype": "Datetime", 
+            "default": frappe.datetime.now_datetime(), 
+            "reqd": 0 
+        },
+	]
+};
